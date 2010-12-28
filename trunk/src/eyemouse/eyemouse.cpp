6517bf66401ef6e21e0df46009e295560642fe81
ajaspe@gmail.com <ajaspe@gmail.com@258eb13e-0373-a46f-d02b-47ac29fd03b0>
2010-12-28 21:08:59 +0000

    Windows mouse controller test added.

---
+++ trunk/src/eyemouse/eyemouse.cpp
@@ -0,0 +1,49 @@
+
+
+#include "../oftalmolib/tobiieyetracker.h"
+#include <iostream>
+#include <windows.h>
+
+#define TET_SERVER  "localhost"    // NULL = default: "127.0.0.1" or "localhost"
+#define PORT        4455       // 0 = default: 4455
+
+
+int main(int argc, char *argv[])
+{
+	OpenThreads::Thread::Init();
+	char *host = TET_SERVER;
+	long port = PORT;
+
+	if ( argc > 1 ) {
+		host = argv[1];
+	}
+	if ( argc > 2 ) {
+		port = atoi(argv[2]);
+	}
+	ot::TobiiEyeTracker *tet = new ot::TobiiEyeTracker( host, port );
+	tet->start();
+	tet->waitForInit();
+
+    OpenThreads::Thread::microSleep(3000000);
+    float x,y;
+	
+	// Lets take clip rectangle for the win mouse
+	RECT screenRect;
+	GetClipCursor(&screenRect);
+	unsigned int width  = screenRect.right - screenRect.left;
+	unsigned int height = screenRect.bottom - screenRect.top;
+	
+	while(true) {
+		x = tet->getCurrentGaze().normX() * width;
+		y = tet->getCurrentGaze().normX() * height;
+		SetCursorPos(x,y);
+        OpenThreads::Thread::microSleep(100000);
+
+#ifdef _DEBUG
+		POINT p;
+		GetCursorPos(&p);
+		std::cout << p.x << "\t" << p.y << std::endl;
+#endif
+
+    }
+}
